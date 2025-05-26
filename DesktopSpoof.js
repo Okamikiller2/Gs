9d5f9e4d92ffb4e5ff2d30b9b4bd862743b71529
Okamikiller2 <funnehwatcher@gmail.com>
2025-05-26 10:30:09 -0700

    Update DesktopSpoof.js
    D

--- DesktopSpoof.js
+++ DesktopSpoof.js
@@ -1,61 +1,42 @@
-const manifest = {
-  name: "desktop-spoof",
+function h(o) { window.enmity.plugins.registerPlugin(o) }
+function g(o) { return window.enmity.patcher.create(o) }
+const t = window.enmity.modules.common.React;
+const f = window.enmity.assets.getIDByName;
+const P = t.Form, a = t.FormRow;
+
+const r = {
+  name: "DesktopSpoof",
   version: "1.0.0",
-  description: "Spoofs Discord into thinking you're using a desktop client (Windows/macOS).",
-  authors: [{ name: "yourname", id: "000000000000000000" }],
+  description: "Spoofs Discord into thinking you're on a desktop client",
+  authors: [{ name: "Oliver", id: "738291045672984576" }]
 };
 
-const settings = window.enmity.settings.makeStore(manifest.name);
-const patcher = window.enmity.patcher.create(manifest.name);
-const React = window.enmity.modules.common.React;
-const { FormRow, FormSwitch, FormSection } = window.enmity.modules.common.components;
-const { getByProps } = window.enmity.modules;
-const { createThemedStyleSheet } = window.enmity.modules.common.StyleSheet;
-const { Constants } = window.enmity.modules.common;
+const I = g("DesktopSpoof");
 
-const spoofValues = {
-  os: "Windows",
-  browser: "Discord",
-  device: "Desktop"
-};
-
-function patchIdentify() {
-  const wsModule = getByProps("identify");
-  if (!wsModule || !wsModule.identify) return;
-
-  patcher.after("identify", wsModule, (_, [data]) => {
-    if (!data?.properties) return;
-    if (!settings.getBoolean("enabled", true)) return;
-
-    data.properties.$os = spoofValues.os;
-    data.properties.$browser = spoofValues.browser;
-    data.properties.$device = spoofValues.device;
-  });
-}
-
-function SettingsPanel() {
-  return React.createElement(FormSection, {
-    title: "Desktop Spoof Settings",
-    children: React.createElement(FormRow, {
-      label: "Enable Spoofing",
-      subLabel: "Toggle to spoof your client as a desktop",
-      trailing: React.createElement(FormSwitch, {
-        value: settings.getBoolean("enabled", true),
-        onValueChange: (val) => settings.set("enabled", val)
-      })
-    })
-  });
-}
-
-window.enmity.plugins.registerPlugin({
-  ...manifest,
+const D = {
+  ...r,
   onStart() {
-    patchIdentify();
+    I.after("identify", window.enmity.modules.getByProps("identify"), (_, [data]) => {
+      if (data?.properties) {
+        data.properties.$os = "Windows";
+        data.properties.$browser = "Discord";
+        data.properties.$device = "Desktop";
+      }
+    });
   },
   onStop() {
-    patcher.unpatchAll();
+    I.unpatchAll();
   },
   getSettingsPanel() {
-    return React.createElement(SettingsPanel);
+    return t.createElement(
+      P, { title: "Desktop Spoof" },
+      t.createElement(a, {
+        label: "Status",
+        subLabel: "This plugin spoofs your client as desktop",
+        leading: t.createElement(a.Icon, { source: f("ic_sync_24px") })
+      })
+    );
   }
-});
+};
+
+h(D);
